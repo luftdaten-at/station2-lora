8297bce49f5f43285d93af768e62a9d65f5b43fa
Silvio Heinze <mail@silvioheinze.eu>
2021-02-18 11:11:32 +0000

    change to MIT License

--- code/main.py
+++ code/main.py
@@ -3,7 +3,7 @@ Luftdaten.at Station v2.3 LoRa ESP32
 Firmware
 
 author: Silvio Heinze (silvio@luftdaten.at)
-license: The GNU General Public License v3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+license: MIT License
 """
 
 from machine import I2C, Pin, UART
